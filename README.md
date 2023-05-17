db35a1600bf3a06bd95923bfc30855a7b71980be
Franz Matthies <fmatthies@users.noreply.github.com>
2023-05-17 16:26:58 +0200

    preprocessing endpoint is working now

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # concept-graphs
+
+## Endpoints
+### `/preprocessing`
+upload text data to be preprocessed (i.e. extraction of phrases)  
+
+`curl -X POST -F data=@"PATH/TO/DATA.zip" -F config=@"PATH/TO/CONFIG.yaml" -F labels=@"PATH/TO/LABELS.yaml" http://SOME_IP:SOME_PORT/preprocessing`  
+
+* `data`  (mandatory) : the text files provided as one zip file
+* `config` (optional) : configurations for the preprocessing step provided as yaml file
+* `labels` (optional) : the label (if any) of each data text file provided as yaml file (line-wise) (e.g. an entry looks like this: `file1: LABEL1`) 
+
+### `/embedding`
+
+### `/clustering`
+
+## Example Config Files (YAML)
+### `/preprocessing`
+```
+# Name of the corpus; can be chosen freely (but acts as reference point between the different endpoint actions)
+corpus_name: default
+# Name of the spaCy model; pre-installed: de_dep_news_trf, de_core_news_sm (German) & en_core_web_trf (English)
+spacy_model: de_dep_news_trf
+# Number of processes that will be spawned (how many cores will be utilized)
+n_process: 1
+# Encoding of the text files
+file_encoding: utf-8
+# Lemmatize the input data
+use_lemma: False
+# Prepend the head of a phrase to its beginning
+prepend_head: False
+# Use only the head of a phrase
+head_only: False
+# Discriminate between upper and lowercase tokens even if it's the same word
+case_sensitive: False
+# If a tf-idf pre-filter for phrases should be used; 1, 1.0, None takes phrases as is; see e.g. scikit-learn's tf-idf vectorizer for a description
+# (int or float)
+filter_min_df: 1
+# (int or float)
+filter_max_df: 1.0
+# (None or name of POSTed stopwords file)
+filter_stop: None
+# Name of spaCy pipeline components to disable (None or array)
+disable: None
+```
--- main.py
+++ main.py
@@ -6,23 +6,21 @@ import sys
 import time
 import os
 import zipfile
+import inspect
 
 from pathlib import Path
-from typing import Optional, Callable, Union
-from collections import Counter, defaultdict
+from typing import Optional, Callable, Union, Dict, Generator, Iterable, List
+from collections import Counter, defaultdict, namedtuple
 
 import yaml
-import pandas as pd
 import spacy
-import ir_datasets
 
-from sklearn.datasets import fetch_20newsgroups
 from flask import Flask, jsonify, request
 from flask.logging import default_handler
 
 sys.path.insert(0, "src")
 # import cluster_functions
-# import data_functions
+import data_functions
 # import embedding_functions
 # import util_functions
 
@@ -31,44 +29,94 @@ app = Flask(__name__)
 root = logging.getLogger()
 root.addHandler(default_handler)
 
-TMP = "./tmp"
+FILE_STORAGE_TMP = "./tmp" #ToDo: replace it with proper path in docker
+# ToDo: file with stopwords will be POSTed: #filter_stop: Optional[list] = None,
 
 @app.route("/preprocessing", methods=['POST'])
 def data_preprocessing():
     app.logger.info("Preprocessing started")
     if request.method == "POST" and len(request.files) > 0 and "data" in request.files:
-        data = request.files.get("data", None)
-        config = request.files.get("config", None)
-        labels = request.files.get("labels", None)
+
+        app.logger.info("Reading config ...")
+        config = read_config(request.files.get("config", None))
+        app.logger.info(f"Parsed the following arguments for preprocessing:\n\t{config}")
+        process_name = config.get("corpus_name",
+                                  request.files.get("data", namedtuple('Corpus', ['name'])("default")).name)
+
+        app.logger.info("Reading labels ...")
+        labels = read_labels(request.files.get("labels", None))
+        app.logger.info(f"Gathered the following labels:\n\t{list(labels.values()) if labels is not None else []}")
 
         app.logger.info("Reading data ...")
-        try:
-            archive_path = f"{TMP}/{data.filename}"
-            data.save(archive_path)
-            with zipfile.ZipFile(archive_path, mode='r') as archive:
-                app.logger.info(read_zip_content(archive).__next__())
-        except Exception as e:
-            app.logger.error(f"Something went wrong with data file reading: {e}")
+        data = read_data(request.files.get("data", None), config, labels)
+        app.logger.info(f"Counted {len(data)} item ins zip file.")
 
-        if config is not None:
-            app.logger.info("Reading config ...")
-            # ToDo: read config yaml
-        else:
-            app.logger.info("No config file provided; using default values")
+        app.logger.info(f"Start preprocessing '{process_name}' ...")
+        start_preprocessing(data, config, process_name)
 
-        # ToDo: start preprocessing with config values on data
+    elif len(request.files) <= 0 or "data" not in request.files:
+        app.logger.error("There were no files or no data files POSTed."
+                         " At least a zip folder with the data is necessary!\n"
+                         "\t\ti.e.: curl -X POST -F data=@\"#SOME/PATH/TO/FILE.zip\"")
     return jsonify("Done.")
 
 
-def read_zip_content(zip_archive):
-    #ToDo: name - split ext according to extension in config
-    #ToDo: content - decode according to encoding in config
-    #ToDo: label - set according to separate label file if present
-    return ({"name": f.filename.split("/")[-1], "content": zip_archive.read(f.filename).decode("utf-8"), "label": None}
-            for f in zip_archive.filelist if not f.is_dir())
+def read_data(data, config, labels) -> List[Dict[str, str]]:
+    try:
+        archive_path = f"{FILE_STORAGE_TMP}/{data.filename}"
+        data.save(archive_path)
+        with zipfile.ZipFile(archive_path, mode='r') as archive:
+            return read_zip_content(archive, config, labels)
+    except Exception as e:
+        app.logger.error(f"Something went wrong with data file reading: {e}")
 
 
-# data in zip format
+def read_config(config) -> Dict[str, str]:
+    base_config = {'pipeline': 'en_core_web_trf', 'file_encoding': 'utf-8'}
+    if config is None:
+        app.logger.info("No config file provided; using default values")
+    else:
+        try:
+            base_config = yaml.safe_load(config.stream)
+        except Exception as e:
+            app.logger.error(f"Couldn't read config file: {e}")
+    return base_config
+
+
+def read_labels(labels) -> Dict[str, str]:
+    base_labels = {}
+    if labels is None:
+        app.logger.info("No labels file provided; no labels will be added to text data")
+    else:
+        try:
+            base_labels = yaml.safe_load(labels.stream)
+        except Exception as e:
+            app.logger.error(f"Couldn't read labels file: {e}")
+    return base_labels
+
+
+def read_zip_content(zip_archive, config, labels) -> List[Dict[str, str]]:
+    return [{"name": Path(f.filename).stem,
+             "content": zip_archive.read(f.filename).decode(config.get('file_encoding', 'utf-8')),
+             "label": labels.get(Path(f.filename).stem, None)}
+            for f in zip_archive.filelist if not f.is_dir()]
+
+
+def start_preprocessing(data, config, cache_name):
+    default_args = inspect.getfullargspec(data_functions.DataProcessingFactory.create)[0]
+    spacy_language = spacy.load(config.pop("spacy_model"))
+    _ = [config.pop(x, None) for x in list(config.keys()) if x not in default_args]
+    data_functions.DataProcessingFactory.create(
+        pipeline=spacy_language,
+        base_data=data,
+        cache_name=f"{cache_name}_data-processed",
+        cache_path=Path(FILE_STORAGE_TMP),
+        save_to_file=True,
+        **config
+    )
+
+
+# label tab separated list of file name (minus its extension like '.txt') with according label
 # config in yaml format
 # curl -X POST -F data=@"LICENSE" -F config=@"conf/preprocessing_config.yaml" -F labels=@"..." http://localhost:9007/preprocessing
 if __name__ == "__main__":
