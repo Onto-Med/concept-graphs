7a9655f20ce1341559e69495896757f53062e4e2
Franz Matthies <franz.matthies@imise.uni-leipzig.de>
2025-09-12 15:48:05 +0200

    something doesn't work when loading models

--- download_models.py
+++ download_models.py
@@ -3,11 +3,14 @@ from transformers import GPT2Tokenizer
 
 models = {
     "spacy": (spacy.cli.download, ["de_core_news_sm", "de_dep_news_trf"],),
-    "transformers": (GPT2Tokenizer.from_pretrained, ["gpt2", "dbmdz/german-gpt2"],)
+    "transformers": (GPT2Tokenizer.from_pretrained, ["gpt2", "dbmdz/german-gpt2"], {"force_download": True},)
 }
 
 if __name__ == '__main__':
     for k, v in models.items():
         _loader = v[0]
         for model in v[1]:
-            _loader(model)
+            if len(v) >= 3:
+                _loader(model, **v[2])
+            else:
+                _loader(model)
